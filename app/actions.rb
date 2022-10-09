375be1ca5e62b8923a5f2da69421ba8d4ea46483
david.detours <dvkornacki@gmail.com>
2022-10-09 02:48:00 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+get '/signup' do
+  @user = User.new
+  erb(:signup)
+end
+
+post '/signup' do
+  # grab user input values from params
+  email = params[:email]
+  avatar_url = params[:avatar_url]
+  username = params[:username]
+  password = params[:password]
+
+  #instantiate and save a User
+  @user = User.new({email: email, avatar_url: avatar_url, username: username, password: password})
+
+  #if all user params are present
+  if @user.save
+
+    "User #{username} saved!"
+
+  else
+
+    erb(:signup)
+    
+  end
+end
