44b46d44eb4687ee439e5bee8374fd1799efc747
david.detours <dvkornacki@gmail.com>
2022-10-13 00:42:57 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -21,7 +27,7 @@ post '/signup' do
   #if all user params are present
   if @user.save
 
-    "User #{username} saved!"
+    redirect to ('/login')
 
   else
 
@@ -29,3 +35,33 @@ post '/signup' do
     
   end
 end
+
+get '/login' do # when a GET request comes into /login
+  erb(:login)   # render app/views/login.erb
+
+end
+
+post '/login' do 
+  username = params[:username]
+  password = params[:password]
+
+@user = User.find_by(username: username)
+
+if @user && @user.password == password
+  session[:user_id] = @user.id
+  redirect to('/')
+
+else
+ @error_message = "Login failed."
+ erb(:login)
+end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+
+
+
